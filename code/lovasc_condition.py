c1b362750111c32a962f6bf55481053bdc900b5c
twhite <140679249+10-12-white@users.noreply.github.com>
2025-12-17 13:15:24 +1100

    Update lovasc_condition.py

--- code/lovasc_condition.py
+++ code/lovasc_condition.py
@@ -1,1 +1,11 @@
+	# lovasc condition and then go from there
+	def check_lovasc(b, b1, mu):
+		basis_vectors = []
+		k = np.round(mu)
+		b_reduced = b - k* b1
+		mu_reduced =  mu - k
+		if (b_reduced * delta)^2 <= gmcoef^2 * b1-1^2 + bi^2:
+			basis_order = basis_vectors
+		return true
 
+# original helper function
